8fc77cdeffdc353a4513496ff85dc535f0cfaade
Luis Yupanqui <sssimple.33@gmail.com>
2019-11-29 04:07:30 -0500

    build

---
+++ build/precache-manifest.6e6e6d68fed328933ef04c7f89c58258.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e458149451836e934e8871c2776657e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0239f5ac74a098d54e41",
+    "url": "/static/css/main.423d3107.chunk.css"
+  },
+  {
+    "revision": "dc5cb2b3eea821ed09b1",
+    "url": "/static/js/2.3c763b3b.chunk.js"
+  },
+  {
+    "revision": "0239f5ac74a098d54e41",
+    "url": "/static/js/main.34b1d24f.chunk.js"
+  },
+  {
+    "revision": "e6e25de14167014e2770",
+    "url": "/static/js/runtime-main.56639fa5.js"
+  },
+  {
+    "revision": "2707e6740e21baccc8d8ddfb41bfca9b",
+    "url": "/static/media/car.2707e674.svg"
+  },
+  {
+    "revision": "7bb7158b06ec271c19f5ec54c36ae3a6",
+    "url": "/static/media/minus.7bb7158b.svg"
+  },
+  {
+    "revision": "511f5b800c4bac7262d035429a108687",
+    "url": "/static/media/plus.511f5b80.svg"
+  },
+  {
+    "revision": "6c1aa4dde43f8bd15a0fe741a42f9a1f",
+    "url": "/static/media/success.6c1aa4dd.svg"
+  },
+  {
+    "revision": "2bfd658f08eb555aee5498c66b6beac7",
+    "url": "/static/media/truck.2bfd658f.svg"
+  }
+]);
